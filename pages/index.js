e48811986cbd4936cbb89d69fc497621127508df
Danny Aziz <daanyaal997@gmail.com>
2021-11-01 21:43:19 +0000

    feat: main page

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,64 @@
-import Head from 'next/head'
+import Head from 'next/head';
+
+import Header from 'components/header';
+import Footer from 'components/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-bg text-white min-h-screen font-SpaceGrotesk">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>gmi.london</title>
+        <link rel="icon" href="/favicon.ico" />\
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <Header />
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex flex-col gap-y-8 pt-12 pb-20 px-12">
+        <h1 className="text-center text-4xl font-bold">Indie London presents gmi.london a web3 event for builders</h1>
+        <button className="bg-secondary h-12 w-1/4 mx-auto rounded-md px-4">
+          <span>I wanna to go to the event</span>
+        </button>
+        <div className="pt-6">
+          <h2 className="text-center text-2xl font-bold">Event</h2>
+          <h1>Info about the community and the event itself</h1>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <div className="pt-6 flex flex-col gap-y-6">
+          <h2 className="text-center text-2xl font-bold">Speakers</h2>
+          <div>
+            <div className="flex flex-row gap-x-6">
+              <div className="bg-gray-400 h-28 w-28"></div>
+              <p>Some words here</p>
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-row gap-x-6">
+              <div className="bg-gray-400 h-28 w-28"></div>
+              <p>Some words here</p>
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-row gap-x-6">
+              <div className="bg-gray-400 h-28 w-28"></div>
+              <p>Some words here</p>
+            </div>
+          </div>
+        </div>
+        {/* <div className="pt-6">
+          <h2 className="text-center text-2xl font-bold">Reserve</h2>
+          <h1>Info about the community and the event itself</h1>
+        </div> */}
+        {/* <div className="pt-6">
+          <h2 className="text-center text-2xl font-bold">NFT</h2>
+          <h1 className="text-center text-4xl font-bold">Coming Soon...</h1>
+        </div> */}
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
