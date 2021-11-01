7acb3d651210c618b603c3bf6760a3c73aca94f0
Danny Aziz <daanyaal997@gmail.com>
2021-11-01 21:43:05 +0000

    chore: setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './lib/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: '#01030D',
+        cta: '#FF715B',
+        secondary: '#3CCE91',
+      },
+      fontFamily: {
+        SpaceGrotesk: ['Space Grotesk', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
